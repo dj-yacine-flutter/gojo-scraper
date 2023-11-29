1ed07313f7bc1be45141999a3d05e4ad4fcc3da6
Djeddi Yacine <yacine2700dj@gmail.com>
2023-11-29 21:27:25 +0100

    remove anime.go and change anime_data fillname

--- anime/data.go
+++ anime/data.go
@@ -48,7 +48,6 @@ func fetchAnimeTitles() {
 			return
 		}
 
-		// Set a user-agent header to mimic a browser
 		req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
 
 		resp, err := http.DefaultClient.Do(req)
